bbc3ce12217f5823e590c556e7f90db3fb7e64f0
Arnaud BODEL <contact@osw3.net>
2019-02-20 12:45:41 +0100

    -

--- private/app/utils/dump.php
+++ private/app/utils/dump.php
@@ -4,8 +4,11 @@
  *
  * @param [mixed] $data 
  */
-function dump( $data ) {
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
+if (!function_exists('dump')) 
+{
+    function dump( $data ) {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    }
 }
